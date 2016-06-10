e4d453643e148e58f868a964f6782e5dcb1ba2d6
R3C0Nx00 <taylor@puckettonline.net>
2016-06-10 02:37:47 -0400

    sitecloner

--- modules/cloning/sitecloner.py
+++ modules/cloning/sitecloner.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
-import os, sys, commands
+import os, sys, socket
 from subprocess import call
+ip = socket.gethostbyname(socket.gethostname())
 
 def cloner():
+  try:
+    site2clone = raw_input("[site] => ")
+    call("wget " + site2clone + " " + "&& mv index.html /var/www/html && service apache2 start", shell=True)
+    print("<END> Browse to {} <END>".format(ip)) 
+  except KeyboardInterrupt:
+    print("\n")
+    sys.exit(0)
+while True:
+  cloner()
