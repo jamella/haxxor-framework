5489fc7f41edb9db34c8fc8c07b51547d8c3cd49
R3C0Nx00 <taylor@puckettonline.net>
2016-06-10 03:01:44 -0400

    sitecloner

--- modules/cloning/sitecloner.py
+++ modules/cloning/sitecloner.py
@@ -5,7 +5,7 @@ from subprocess import call
 def cloner():
   try:
     site2clone = raw_input("[site] => ")
-    call("wget " + site2clone + " " + " > index.html && mv index.html /var/www/html && service apache2 start", shell=True)
+    call("wget " + site2clone + " " + " -O index.html && mv index.html /var/www/html && service apache2 start", shell=True)
     print("<END> DONE. BROWSE TO YOUR IP <END>")
   except KeyboardInterrupt:
     print("\n")
