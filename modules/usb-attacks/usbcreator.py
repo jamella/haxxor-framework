2f63c45f6b9df20adf9d5f551523ca6980b6eece
R3C0Nx00 <taylor@puckettonline.net>
2016-06-10 06:07:04 -0400

    NOT FINISHED

--- modules/usb-attacks/usbcreator.py
+++ modules/usb-attacks/usbcreator.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
-import pyusb
+import os, time, sys, commands
+from subprocess import call
+try:
+  import pyusb
+except ImportError:
+  print("[!] You are missing pyusb, install it to use usbcreator")
+  sys.exit(0)
+
+def
