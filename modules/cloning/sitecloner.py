c7a2420a364ef86948542a70b6a9cf40ed765de6
R3C0Nx00 <taylor@puckettonline.net>
2016-06-10 03:05:32 -0400

    sitecloner

--- modules/cloning/sitecloner.py
+++ modules/cloning/sitecloner.py
@@ -5,10 +5,17 @@ from subprocess import call
 def cloner():
   try:
     site2clone = raw_input("[site] => ")
-    call("wget " + site2clone + " " + " -O index.html && mv index.html /var/www/html && service apache2 start", shell=True)
-    print("<END> DONE. BROWSE TO YOUR IP <END>")
+    if(site2clone != 'exit'):
+      call("wget " + site2clone + " " + " -O index.html && mv index.html /var/www/html && service apache2 start", shell=True)
+      print("<END> DONE. BROWSE TO YOUR IP <END>")
+    else:
+      leave_apache_running = raw_input("[?] Leave apache2 running [yes/no]: ")
+      if(leave_apache_running == 'yes'):
+        sys.exit(0)
+      else:
+        call("service apache2 stop && rm /var/www/html/index.html", shell=True)
+        sys.exit(0)
   except KeyboardInterrupt:
-    print("\n")
-    sys.exit(0)
+    print("\n Type 'exit' to exit")
 while True:
   cloner()
