65f6d3a520d1546dc4381a741af3d4d92e983727
R3C0Nx00 <taylor@puckettonline.net>
2016-06-12 17:46:35 +0000

    portscan

--- modules/scanning/portscan.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python 
-from socket import *
-import sys, time
-from datetime import datetime
-while True:
-  min_port = raw_input("[>] Minimum port: ")
-  max_port = raw_input("[>] Maximum port: ")
